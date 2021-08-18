40b3a6478ffd699d0611a4f2ffdf02f883b93eb8
Сергей <7atom7@mail.ru>
2021-08-18 15:25:12 +0300

    Reload

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'cni(48#_*bkk2c57w=^k=bstna01859hz3p(xj@2ioie1msc('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['130.193.54.244', 'backend', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['84.201.179.137', 'backend', 'localhost', '127.0.0.1']
 
 
 # Application definition
