4631eb0568cdd880b3fb5b30d0ea5c5df4c04514
Сергей <7atom7@mail.ru>
2021-08-24 17:04:03 +0300

    Users-FIX

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,8 +3,9 @@ from django.urls import include, path
 from .views import FollowApiView, FollowListApiView
 
 urlpatterns = [
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
     path('users/<int:following_id>/subscribe/', FollowApiView.as_view()),
     path('users/subscriptions/', FollowListApiView.as_view()),
     path('', include('djoser.urls')),
-    path(r'^auth/', include('djoser.urls.authtoken')),
 ]
