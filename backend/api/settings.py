151e75deac3bee2a012716ec5780a1dd3721baee
Сергей <7atom7@mail.ru>
2021-08-24 23:14:45 +0300

    Users fix v 1.6

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -169,7 +169,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
