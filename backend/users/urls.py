2278353181e8521f449b6354fb483f853550a157
Сергей <7atom7@mail.ru>
2021-08-24 16:59:20 +0300

    Frontend-DONE, users fix

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('users/<int:following_id>/subscribe/', FollowApiView.as_view()),
     path('users/subscriptions/', FollowListApiView.as_view()),
     path('', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
+    path(r'^auth/', include('djoser.urls.authtoken')),
 ]
