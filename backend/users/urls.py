16de32537261d23956c30247987378b6de715244
Сергей <7atom7@mail.ru>
2021-08-24 22:36:30 +0300

    Users auth error v.1.1

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,8 +3,8 @@ from django.urls import include, path
 from .views import FollowApiView, FollowListApiView
 
 urlpatterns = [
-    path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
+    path('auth/', include('djoser.urls')),
     path('users/<int:following_id>/subscribe/', FollowApiView.as_view()),
     path('users/subscriptions/', FollowListApiView.as_view()),
     path('', include('djoser.urls')),
