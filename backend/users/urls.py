e489844aae5649a593d29190dc5685ec991d36b2
Сергей <7atom7@mail.ru>
2021-08-24 23:09:22 +0300

    Users fix v 1.5

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,11 +1,11 @@
-from django.urls import include, path
+from django.urls import include, path, re_path
 
 from .views import FollowApiView, FollowListApiView
 
 urlpatterns = [
-    path('auth/', include('djoser.urls.authtoken')),
-    path('auth/', include('djoser.urls')),
+    # path('auth/', include('djoser.urls')),
     path('users/<int:following_id>/subscribe/', FollowApiView.as_view()),
     path('users/subscriptions/', FollowListApiView.as_view()),
     path('', include('djoser.urls')),
+    re_path(r'^auth/', include('djoser.urls.authtoken')),
 ]
