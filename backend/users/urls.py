74426ef2685476ae88114e6c761ad762390d3b37
Сергей <7atom7@mail.ru>
2021-08-25 09:17:10 +0300

    Users fix login

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,4 +1,6 @@
-from django.urls import include, path, re_path
+from django.urls import include, path
+
+from djoser import views as views_dj
 
 from .views import FollowApiView, FollowListApiView
 
@@ -7,5 +9,14 @@ urlpatterns = [
     path('users/<int:following_id>/subscribe/', FollowApiView.as_view()),
     path('users/subscriptions/', FollowListApiView.as_view()),
     path('', include('djoser.urls')),
-    re_path(r'^users/auth/', include('djoser.urls.authtoken')),
+    path(
+        'api/auth/token/login/',
+        views_dj.TokenCreateView.as_view(),
+        name='login'
+    ),
+    path(
+        'api/auth/token/logout/',
+        views_dj.TokenDestroyView.as_view(),
+        name='login'
+    )
 ]
