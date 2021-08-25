b595f704044d377c99a3230483849ba5864415d0
Сергей <7atom7@mail.ru>
2021-08-25 23:00:16 +0300

    Token auth fix v 1.1

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -125,6 +125,12 @@ DATABASES = {
         'PORT': os.environ.get('DB_PORT', default='5432'),
     }
 }
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
