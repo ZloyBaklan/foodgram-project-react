d3b54d9a8e07e40a6a28b54032b1854ec6761ba9
Сергей <7atom7@mail.ru>
2021-08-27 23:34:42 +0300

    Users from admin login - OK, users fix v.2

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -94,6 +94,7 @@ class CurrentUserSerializer(UserCreateSerializer):
             'first_name',
             'last_name',
             'is_subscribed',
+            'password'
         )
 
     def create(self, validated_data):
