bd400c037d603ce9fc1a1dc6aeae18e9f9dfff95
ZloyBaklan <zloybaklan@MSI.localdomain>
2021-08-29 20:52:41 +0300

    Ubuntu...

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -110,7 +110,7 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -121,14 +121,14 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': os.environ.get('DB_ENGINE'),
@@ -139,6 +139,7 @@ DATABASES = {
         'PORT': os.environ.get('DB_PORT'),
     }
 }
+'''
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
