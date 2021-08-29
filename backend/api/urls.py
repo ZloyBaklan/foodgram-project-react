02adeec92b69bc8567b014f0a3744763a312cf7a
Sergey <75798404+ZloyBaklan@users.noreply.github.com>
2021-08-29 16:29:08 +0300

    Update urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,9 +4,6 @@ The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
@@ -22,16 +19,14 @@ from drf_spectacular.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
-    path('api/', include('recipes.urls')),
-    path('api/', include('ingredients.urls')),
     path('api/', include('tags.urls')),
+    path('api/', include('recipes.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path(
         'api/schema/redoc/',
         SpectacularRedocView.as_view(url_name='schema'),
         name='redoc'
     ),
-    # Путь для спеки от Swagger — тут можно делать запросы как в Postman!
     path(
         'api/schema/swagger-ui/',
         SpectacularSwaggerView.as_view(url_name='schema'),
