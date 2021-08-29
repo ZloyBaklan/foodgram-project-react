e9cea15d6e6ab63f9e9b679d28063f0f4d49d954
Sergey <75798404+ZloyBaklan@users.noreply.github.com>
2021-08-29 16:45:47 +0300

    BigFix

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -121,14 +121,24 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get('DB_ENGINE'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
