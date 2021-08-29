407598c09577da1c7774803e84cad2f541290da4
Sergey <75798404+ZloyBaklan@users.noreply.github.com>
2021-08-29 16:33:55 +0300

    BigFix

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,11 +1,26 @@
 from django.core.validators import MinValueValidator
 from django.db import models
 from django.db.models import UniqueConstraint
-from ingredients.models import Ingredient
+
 from tags.models import Tag
 from users.models import CustomUser
 
 User = CustomUser
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200,
+                            verbose_name='Название ингредиента', null=False)
+    measurement_unit = models.CharField(max_length=20,
+                                        verbose_name='Единица измерения',
+                                        null=False)
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        ordering = ['name']
+
+    def __str__(self):
+        return f'{self.name}, {self.measurement_unit}'
 
 
 class Recipe(models.Model):
@@ -16,7 +31,7 @@ class Recipe(models.Model):
                                          related_name='ingredients',
                                          through='IngredientAmount',
                                          verbose_name='Ингредиенты')
-    tag = models.ManyToManyField(Tag, related_name='tags',
+    tags = models.ManyToManyField(Tag, related_name='tags',
                                  verbose_name='Хэштег')
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True,
                                     db_index=True)
@@ -24,10 +39,9 @@ class Recipe(models.Model):
                             max_length=1000)
     name = models.CharField(max_length=200, verbose_name='Название',
                             null=False)
-    image = models.ImageField(upload_to='media/', verbose_name='Изображение',
-                              blank=True, null=True)
-    cooking_time = models.IntegerField(
-        default=0, verbose_name='Время готовки в минутах',
+    image = models.ImageField(upload_to='media/', verbose_name='Изображение')
+    cooking_time = models.PositiveIntegerField(
+        default=1, verbose_name='Время готовки в минутах',
         blank=True, validators=[MinValueValidator(1)]
     )
 
@@ -42,14 +56,18 @@ class Recipe(models.Model):
 class Favorite(models.Model):
     user = models.ForeignKey(User, related_name='favorites',
                              on_delete=models.CASCADE)
-    favorite = models.ForeignKey(Recipe, related_name='favorites',
+    recipe = models.ForeignKey(Recipe, related_name='favorites',
                                  on_delete=models.CASCADE)
+    added = models.DateTimeField(
+        auto_now_add=True, verbose_name='Дата добавления в избранное'
+    )
 
     class Meta:
-        UniqueConstraint(fields=['favorite', 'user'], name='favorite_unique')
+        verbose_name = 'Избранное'
+        UniqueConstraint(fields=['recipe', 'user'], name='favorite_unique')
 
     def __str__(self):
-        return f"{self.user} has favorites: {self.favorite.name}"
+        return f"{self.user} has favorites: {self.recipe.name}"
 
 
 class ShoppingList(models.Model):
@@ -59,7 +77,12 @@ class ShoppingList(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE,
                                related_name='purchases',
                                verbose_name='Покупка')
-    added_time = models.DateTimeField(verbose_name='Дата добавления')
+    added = models.DateTimeField(
+        auto_now_add=True, verbose_name='Дата добавления в список покупок'
+    )
+
+    class Meta:
+        verbose_name = 'Покупки'
 
     def __str__(self):
         return f'In {self.user} shopping list: {self.recipe}'
@@ -78,6 +101,9 @@ class IngredientAmount(models.Model):
         default=1, validators=[MinValueValidator(1)],
         verbose_name='Количество ингредиентов'
     )
+
+    class Meta:
+        verbose_name = 'Количество'
 
     def __str__(self):
         return f'{self.ingredient} in {self.recipe}'
