911ef2c09715c9df0145676efd893edbf917279f
Sergey <75798404+ZloyBaklan@users.noreply.github.com>
2021-08-29 16:31:14 +0300

    BigFix

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,6 +1,46 @@
 from django.contrib import admin
 
-from .models import Favorite, Recipe
+from .models import (Favorite, Recipe, Ingredient,
+                     ShoppingList, IngredientAmount)
 
-admin.site.register(Recipe)
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'measurement_unit'
+    )
+    search_fields = ('name',)
+    empty_value_display = '-пусто-'
+
+
+class IngredientsInline(admin.TabularInline):
+    model = Ingredient
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'author',
+        'name',
+        'image',
+        'text',
+        'is_favorited',
+        'ingredients',
+    )
+    search_fields = ('author', 'name')
+    list_filter = ('author', 'name', 'tags')
+    empty_value_display = '-пусто-'
+
+    def is_favorited(self, obj):
+        return obj.favorites.count()
+
+    def ingredients(self, obj):
+        return list(obj.ingredients.all())
+    ingredients.short_description = 'Ингредиенты'
+
+admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Favorite)
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(IngredientAmount)
+admin.site.register(ShoppingList)
