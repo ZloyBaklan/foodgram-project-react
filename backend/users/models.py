15bfd608d00e4f6b626897d52f4c4d7047c1caab
Sergey <75798404+ZloyBaklan@users.noreply.github.com>
2021-08-29 16:44:12 +0300

    BigFix

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models import UniqueConstraint
-from django.utils.translation import gettext_lazy as _
-from django.utils import timezone
+# from django.utils.translation import gettext_lazy as _
+# from django.utils import timezone
 
 # from .managers import CustomUserManager
 
