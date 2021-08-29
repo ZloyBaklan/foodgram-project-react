0395758ce194699bb4bfd4310b7a6e01175dcbaf
ZloyBaklan <zloybaklan@MSI.localdomain>
2021-08-30 00:11:42 +0300

    ubuntu

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -110,7 +110,7 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -121,14 +121,14 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': os.environ.get('DB_ENGINE'),
@@ -139,7 +139,7 @@ DATABASES = {
         'PORT': os.environ.get('DB_PORT'),
     }
 }
-'''
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -199,10 +199,11 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100,
 }
-
+'''
 # подключаем движок filebased.EmailBackend
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 # указываем директорию, в которую будут складываться файлы писем
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
 
 ADMIN_EMAIL = '<admin@foodgram.ru>'
+'''
