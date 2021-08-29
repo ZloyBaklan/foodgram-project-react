802a91380c19eff97f4a2b0f75da09dd26b56023
Sergey <75798404+ZloyBaklan@users.noreply.github.com>
2021-08-29 19:07:48 +0300

    BigFix

--- django_entrypoint.sh
+++ django_entrypoint.sh
@@ -16,14 +16,22 @@ if [ "$POSTGRES_USER" = "postgres" ]; then
 fi
 
 # Запускаем миграции, загружаем фикстуры, собираем и сжимаем статику
+python manage.py makemigrations --noinput
+
 echo "Making migrations."
 python manage.py migrate --noinput
 
 echo "Loading demo data from fixtures.json"
-python manage.py loaddata -i fixtures.json
+python manage.py loaddata -i ingredients.json
 
 echo "Collecting static files."
 python manage.py collectstatic --noinput
 
 echo "Compressing static files."
 python manage.py compress
+
+# Запускаем gunicorn на нашем $PORT
+echo "Starting gunicorn"
+gunicorn api.wsgi:application --bind 0.0.0.0:8000 \
+
+exec "$@"
