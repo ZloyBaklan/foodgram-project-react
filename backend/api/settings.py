3c39e39cda67afa7227e10a9dfd2055a612f636f
Сергей <7atom7@mail.ru>
2021-08-30 20:50:34 +0300

    db

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -140,11 +140,11 @@ DATABASES = {
         'ENGINE': os.environ.get(
             'DB_ENGINE', default='django.db.backends.postgresql'
         ),
-        'NAME': os.environ.get('DB_NAME', default='postgres'),
-        'USER': os.environ.get('POSTGRES_USER', default='postgres'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', default='postgres'),
-        'HOST': os.environ.get('DB_HOST', default='db'),
-        'PORT': os.environ.get('DB_PORT', default='5432'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
