0dc9a0893f1a43697e9d079f1e412c893a9f340b
Сергей <7atom7@mail.ru>
2021-08-31 21:25:17 +0300

    Tags fix

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -28,11 +28,30 @@ class IngredientView(viewsets.ReadOnlyModelViewSet):
 
 class RecipeViewSet(ModelViewSet):
     permission_classes = [IsOwnerOrReadOnly, ]
-    queryset = Recipe.objects.all()
     pagination_class = PageNumberPagination
     pagination_class.page_size = 6
     filter_backends = [DjangoFilterBackend, ]
     filterset_class = RecipeFilter
+
+    def get_queryset(self):
+        queryset = Recipe.objects.all()
+        user = self.request.user
+        is_in_shopping_cart = self.request.query_params.get(
+            "is_in_shopping_cart"
+        )
+        is_favorited = self.request.query_params.get("is_favorited")
+        shopping_list = ShoppingList.objects.filter(user=user)
+        favorite = Favorite.objects.filter(user=user)
+
+        if is_in_shopping_cart == "true":
+            queryset = queryset.filter(shopping_cart__in=shopping_list)
+        elif is_in_shopping_cart == "false":
+            queryset = queryset.exclude(shopping_cart__in=shopping_list)
+        if is_favorited == "true":
+            queryset = queryset.filter(favorites__in=favorite)
+        elif is_favorited == "false":
+            queryset = queryset.exclude(favorites__in=favorite)
+        return queryset.all()
 
     def get_serializer_class(self):
         if self.request.method in ('POST', 'PUT', 'PATCH'):
